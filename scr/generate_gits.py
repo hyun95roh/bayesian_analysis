c0a753e8e47b0e85c85b2dbb044ff7b3f4124c1e
hyun95roh <hroh@usc.edu>
2025-10-14 02:10:39 -0700

    Demo update

---
+++ scr/generate_gits.py
@@ -0,0 +1,6 @@
+# scripts/generate_gifs.py
+# Python script using matplotlib + imageio to create GIFs demonstrating posterior mean convergence.
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import imageio
